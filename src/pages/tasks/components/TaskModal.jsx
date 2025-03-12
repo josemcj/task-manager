8169d3d83f644e242df5e72b93125cd5688205e1
Jose Contreras <mexidios.mx@gmail.com>
2025-03-12 16:49:38 -0600

    feat: Agrega modal de edicion y creacion

---
+++ src/pages/tasks/components/TaskModal.jsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from 'react';
+import { useTasks } from 'hooks/useTasks';
+import taskStatuses from '@/data/taskStatuses.json';
+import Button from 'components/Button';
+import Modal from 'components/Modal';
+
+const STYLES = {
+  input:
+    'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 focus:outline-hidden block w-full p-2.5',
+  select:
+    'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 focus:outline-hidden block w-full p-2.5',
+};
+
+function TaskModal({ open, onClose, taskToEdit }) {
+  const tasks = useTasks();
+
+  const [task, setTask] = useState({
+    title: '',
+    description: '',
+    status: '',
+  });
+
+  useEffect(() => {
+    if (taskToEdit) setTask(taskToEdit);
+  }, [taskToEdit]);
+
+  const resetForm = () => {
+    setTask({
+      title: '',
+      description: '',
+      status: '',
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    tasks.save(task);
+    resetForm();
+    onClose();
+  };
+
+  return (
+    <Modal title={`${taskToEdit ? 'Editar' : 'Agregar'} tarea`} open={open} onClose={onClose}>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="name" className="block mb-2 font-medium text-gray-900">
+            Nombre
+          </label>
+          <input
+            type="text"
+            id="name"
+            className={STYLES['input']}
+            placeholder="Nombre de la tarea"
+            required
+            value={task.title}
+            onChange={(e) => setTask({ ...task, title: e.target.value })}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="description" className="block mb-2 font-medium text-gray-900">
+            Descripción
+          </label>
+          <textarea
+            id="description"
+            className={STYLES['input']}
+            placeholder="Agrega una descripción"
+            required
+            value={task.description}
+            onChange={(e) => setTask({ ...task, description: e.target.value })}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="status" className="block mb-2 font-medium text-gray-900">
+            Selecciona el estado
+          </label>
+          <select
+            id="status"
+            className={STYLES['select']}
+            value={task.status}
+            onChange={(e) => setTask({ ...task, status: e.target.value })}>
+            <option value="" disabled>
+              Seleccionar
+            </option>
+
+            {taskStatuses.map((status) => (
+              <option key={status.id} value={status.id}>
+                {status.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="flex justify-end">
+          <Button type="submit">Guardar</Button>
+        </div>
+      </form>
+    </Modal>
+  );
+}
+
+export default TaskModal;
--- src/pages/tasks/index.jsx
+++ src/pages/tasks/index.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react';
 import { useTasks } from 'hooks/useTasks';
+import TaskModal from './components/TaskModal';
 import TaskCard from './components/TaskCard';
-import TaskForm from './components/TaskForm';
 
 function Tasks() {
   const tasks = useTasks();
+  const [showTaskModal, setShowTaskModal] = useState(false);
   const [taskToEdit, setTaskToEdit] = useState(null);
 
   const handleEdit = (item) => {
     setTaskToEdit(item);
+    setShowTaskModal(true);
   };
 
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="container mx-auto">
-        {/* <TaskForm taskToEdit={taskToEdit} /> */}
+      <TaskModal open={showTaskModal} onClose={() => setShowTaskModal(false)} taskToEdit={taskToEdit} />
 
+      <div className="container mx-auto">
         {tasks.all().length > 0 ? (
           <div className="grid grid-cols-3 gap-4">
             {tasks.all().map((task) => (
