2ebe97282f754022bfb6dc4dfef4465ec8279116
José Contreras <desarrollo.11@nexen-elog.com>
2025-03-13 10:11:54 -0600

    feat: agrega icono en titulo de pagina y modifica color en borde de card

--- src/pages/tasks/components/TaskCard.jsx
+++ src/pages/tasks/components/TaskCard.jsx
@@ -1,18 +1,34 @@
+import { useState, useEffect } from 'react';
 import { PencilIcon, TrashIcon } from '@heroicons/react/24/solid';
 import { useTasks } from 'hooks/useTasks';
+import taskStatuses from '@/data/taskStatuses.json';
+import Badge from 'components/Badge';
 import Button from 'components/Button';
 
 function TaskCard({ task, onClick }) {
   const tasks = useTasks();
+  const [statusText, setStatusText] = useState('');
+
+  const BADGE_TYPES = {
+    1: 'secondary',
+    2: 'warning',
+    3: 'success',
+  };
 
   const handleDelete = (id) => {
     tasks.deleteById(id);
   };
 
+  useEffect(() => {
+    const { name } = taskStatuses.find((status) => status.id == task.status);
+    setStatusText(name);
+  }, [task]);
+
   return (
-    <div className="p-8 border border-gray-300 dark:border-gray-700 rounded-2xl flex flex-col dark:bg-gray-800">
-      <div className="mb-2">
-        <h3 className="font-bold text-xl dark:text-gray-100">{task.title}</h3>
+    <div className="p-8 border border-gray-400 dark:border-gray-700 rounded-2xl flex flex-col dark:bg-gray-800">
+      <div className="mb-3">
+        <h3 className="font-bold text-xl dark:text-gray-100 mb-3">{task.title}</h3>
+        <Badge text={statusText} type={BADGE_TYPES[task.status]} />
       </div>
 
       <div className="mb-3">
